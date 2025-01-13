2ac8fd5a242e09e5a393842bda7e8865a5b82e40
Tayyip GUZEL <tguzeldev@gmail.com>
2025-01-13 10:03:42 +0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Tayyip Güzel on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoAppTests/TodoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTests.swift
+//  TodoAppTests
+//
+//  Created by Tayyip Güzel on 13.01.2025.
+//
+
+import Testing
+@testable import TodoApp
+
+struct TodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
